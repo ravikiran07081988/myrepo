d728628813c4a1bde39e2de428733cfdf1e5329b
Ravikiran Narasimhamurthy <ravikiran07081988@gmail.com>
2019-10-15 22:07:40 +0530

    test1

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const path = require('path');
  
 const app = express();
  
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/git'));
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/angularapp'));
  
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/git/index.html'));
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
+ 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
