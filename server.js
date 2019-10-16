4d4be31bc02b39c4084a63073c5f64d3781c6def
Ravikiran Narasimhamurthy <ravikiran07081988@gmail.com>
2019-10-16 22:46:30 +0530

    qq

--- server.js
+++ server.js
@@ -26,4 +26,6 @@ res.sendFile(path.join(__dirname+'/angularapp/index.html'));
  
 // Start the app by listening on the default Heroku port
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+	console.log('listening');
+});
