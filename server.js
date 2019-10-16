09e458dcd65b198e32c282db63c217b66f72789d
Ravikiran Narasimhamurthy <ravikiran07081988@gmail.com>
2019-10-16 22:09:52 +0530

    dsds

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const ObjectId = require("mongodb").ObjectID;
 const CONNECTION_URL = "mongodb+srv://admin:admin@rlcluster-bnavh.mongodb.net/test?retryWrites=true&w=majority";
 const DATABASE_NAME = "newdb";
 const app = express();
+const app1 = express();
  
 // Serve only the static files form the angularapp directory
 app.use(express.static(__dirname + '/angularapp'));
  
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
+app1.use(BodyParser.json());
+app1.use(BodyParser.urlencoded({ extended: true }));
 
 app.get('/*', function(req,res) {
  
@@ -21,7 +22,10 @@ res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, () => {
+
+app.listen(process.env.PORT || 8080);
+
+app1.listen(3000, () => {
 	MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
         if(error) {
             throw error;
